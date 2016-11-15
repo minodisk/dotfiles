dd1be2f8548d1965f2a8755f3d3d269bf4362df6
minodisk <daisuke.mino@gmail.com>
2016-11-15 12:13:42 +0900

    Add configu for neovim

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 shopt -s dotglob
-for f in $(pwd)/*; do
-  ln -fs $f ~
+for path in $(pwd)/*; do
+  base=$(basename $path)
+  if [[ $base == ".git" || $base == "deploy.sh" ]]; then
+    continue
+  fi
+  ln -fs $path ~
 done
